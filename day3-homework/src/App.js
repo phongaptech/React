3df670491e43b6c8721b898797ff75b14b6bfa74
phongaptech <phong.bg.1857@aptechlearning.edu.vn>
2025-08-12 00:09:32 +0700

    Add files via upload

--- day3-homework/src/App.js
+++ day3-homework/src/App.js
@@ -5,31 +5,29 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
   const addTask = () => {
-    if (newTask.trim() === "") return;
-    setTasks([...tasks, { text: newTask, completed: false }]);
+    setTasks(tasks.concat({ text: newTask }));
     setNewTask("");
   };
-  const toggleTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks[index].completed = !newTasks[index].completed;
+  const deleteTask = (index) => {
+    const newTasks = tasks;
+    newTasks.splice(index, 1);
     setTasks(newTasks);
   };
-  const deleteTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(setTasks);
-  };
   return (
-    <div className="App" style={{ padding: "20px" }}>
+    <div className="App">
       <h1>Todo App</h1>
-      <input type="text" value={newTask} placeholder="Nhập công việc..." onChange={(e) => setNewTask(e.target.value)} />
+      <input
+        type="text"
+        value={newTask}
+        placeholder="Nhập công việc..."
+        onChange={(e) => setNewTask(e.target.value)}
+      />
       <button onClick={addTask}>Thêm</button>
       <ul>
         {tasks.map((task, index) => (
-          <li key={{ index }} style={{ textDecoration: task.completed ? "line-through" : "none", }}>
-            <span onClick={() => toggleTask(index)} style={{ cursor: "pointer "}}>
-              {task.text}
-            </span>
-            <button className="DltBtn" onClick={() => deleteTask(index)} style={{ marginLeft: "10px" }}>Xóa</button>
+          <li key={index}>
+            {task.text}
+            <button className="delete-btn" onClick={() => deleteTask(index)}>Xóa</button>
           </li>
         ))}
       </ul>
